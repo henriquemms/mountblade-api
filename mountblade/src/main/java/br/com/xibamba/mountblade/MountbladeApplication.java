dd113571d8ea8e5d292d6af7655aac7c6bf5e436
Henrique Machado <henrique_mms@hotmail.com>
2020-06-11 09:20:32 -0300

    Base
    Commit inicial para subir a estrutura do projeto

---
+++ mountblade/src/main/java/br/com/xibamba/mountblade/MountbladeApplication.java
@@ -0,0 +1,13 @@
+package br.com.xibamba.mountblade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountbladeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountbladeApplication.class, args);
+	}
+
+}
---
+++ mountblade/src/test/java/br/com/xibamba/mountblade/MountbladeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xibamba.mountblade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountbladeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
