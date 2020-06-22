0493bf2db7183dcd69479414eeb69e653072ce80
Henrique Machado <henrique_mms@hotmail.com>
2020-06-22 18:57:10 -0300

    Inclusão do create product

--- mountblade/src/main/java/br/com/xibamba/mountblade/controller/ProductController.java
+++ mountblade/src/main/java/br/com/xibamba/mountblade/controller/ProductController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.xibamba.mountblade.model.dto.ProductCreateDto;
 import br.com.xibamba.mountblade.model.entity.Product;
 import br.com.xibamba.mountblade.service.ProductService;
 import io.swagger.annotations.ApiOperation;
@@ -20,8 +23,18 @@ public class ProductController {
 	
 	@GetMapping("/list-product")
 	@ApiOperation("Lista os produtos")
-	public List<Product> listProduct(){
-		return productService.getProduct();
+	public List<Product> listProduct(/*@RequestBody String status*/){
+		return productService.getProduct(/*status*/);
+	}
+	
+	@PostMapping("create")
+	@ApiOperation("Cria um produto")
+	public Product create(@RequestBody ProductCreateDto productCreateDto) {
+		Product product = productCreateDto.productCreateDto();
+		
+		this.productService.createProduct(product);
+		
+		return product;
 	}
 
 }
--- mountblade/src/main/java/br/com/xibamba/mountblade/service/ProductService.java
+++ mountblade/src/main/java/br/com/xibamba/mountblade/service/ProductService.java
@@ -1,7 +1,9 @@
 package br.com.xibamba.mountblade.service;
 
+import java.util.Date;
 import java.util.List;
 
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
@@ -11,20 +13,40 @@ import br.com.xibamba.mountblade.repository.ProductRepository;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
-	public List<Product> getProduct(){
-		
+
+	public List<Product> getProduct(/*String status*/){
+
 		Product product = new Product();
-		
+		/*
+		switch(status) {
+		case "false":
+			product.getDataControl().setDeleted(false);
+			break;
+		case "true":
+			product.getDataControl().setDeleted(true);
+			break;
+		default:
+			product.getDataControl().setDeleted(false);
+		}
+*/		
 		product.getDataControl().setDeleted(false);
-		
 		Example<Product> example = Example.of(product);
-		
+
 		return this.productRepository.findAll(example);
-		
+
 	}
 
+	public Product createProduct(Product product) {
+		if(product.getId() != null) {
+			throw new ServiceException("Não é possível salvar, pois o id está preenchido.");
+		}
+		
+		product.getDataControl().markCreated(new Date());
+		this.productRepository.save(product);
+		
+		return product;
+	};
 }
